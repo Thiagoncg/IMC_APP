de9d37459556860bc80e142ec8c2eb0a2a78fed3
ThiagoGarcia <thiagoncg1@gmail.com>
2022-04-27 15:16:25 -0300

    Createde characters
    Created characters and slider to ilustrate the imc result

--- IMC/Assets/Imc/Scripts/ImcCalculator.cs
+++ IMC/Assets/Imc/Scripts/ImcCalculator.cs
@@ -14,7 +14,15 @@ public class ImcCalculator : MonoBehaviour
     [SerializeField] private InputField inputWeight, inputHeight;
     [SerializeField] private Text textImcResultDisplay, textImcDescription;
     [SerializeField] private Slider sliderWeight, sliderHeight;
+    [SerializeField] private Image character;
+    [SerializeField] private Sprite[] characterResult;
+    [SerializeField] private Image imageSlider;
 
+
+    void Start() 
+    {
+        character.GetComponent<Image>().sprite = characterResult[0];
+    }
     public void CalculatorImc()
     {
         // _weight = float.Parse(inputWeight.text);
@@ -33,33 +41,62 @@ public class ImcCalculator : MonoBehaviour
     {
         if (_imcResult < 18.5)
         {
+
             textImcDescription.text = "Peso abaixo do normal \n Você está abaixo do peso ideal \n Procure se alimentar melhor e praticar exercícios físicos";
             Debug.Log("Peso abaixo do normal");
+
+            character.GetComponent<Image>().sprite = characterResult[0];
+            imageSlider.fillAmount = _imcResult / 100;
+
         }
-        else if ((_imcResult > 18.5) && (_imcResult < 25))
+        else if ((_imcResult > 18.5) && (_imcResult < 25))        
         {
+
             textImcDescription.text = "Peso normal \n Parabéns, você esta no seu peso ideal, procure se manter assim";
             Debug.Log("Peso normal");
+
+            character.GetComponent<Image>().sprite = characterResult[1];
+            imageSlider.fillAmount = _imcResult / 100;
         }
         else if ((_imcResult > 25) && (_imcResult < 30))
         {
+            
             textImcDescription.text = "Sobre o Peso \n Você está acima do peso! \n Procure se alimentar melhor e praticar exercícios físicos";
             Debug.Log("Sobre o Peso");
+
+            character.GetComponent<Image>().sprite = characterResult[2];
+            imageSlider.fillAmount = _imcResult / 100;
+
         }
         else if ((_imcResult > 30) && (_imcResult < 35))
         {
+
             textImcDescription.text = "Grau de Obesidade I \n Você está acima do peso! \n Procure se alimentar melhor e praticar exercícios físicos";
             Debug.Log("Grau de Obesidade I");
+
+            character.GetComponent<Image>().sprite = characterResult[3];
+            imageSlider.fillAmount = _imcResult / 100;
+
         }
         else if ((_imcResult > 35) && (_imcResult < 40))
         {
+
             textImcDescription.text = "Grau de Obesidade II \n Você está acima do peso! \n Procure se alimentar melhor e praticar exercícios físicos";
             Debug.Log("Grau de Obesidade II");
+
+            character.GetComponent<Image>().sprite = characterResult[4];
+            imageSlider.fillAmount = _imcResult / 100;
+
         }
         else if (_imcResult > 40)
         {
+
             textImcDescription.text = "Obesidade Grau III \n Você está acima do peso! \n Procure se alimentar melhor e praticar exercícios físicos";
             Debug.Log("Obesidade Grau III");
+
+            character.GetComponent<Image>().sprite = characterResult[5];
+            imageSlider.fillAmount = _imcResult / 100;
+
         }
 
         textImcResultDisplay.text = (_imcResult.ToString("F") + "Kg/m²");
